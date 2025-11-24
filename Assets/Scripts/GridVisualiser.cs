0aa77688869f77f8223bbcdad2ffdee049744ea2
Faisa-l <146775603+Faisa-l@users.noreply.github.com>
2025-11-24 20:58:23 +0000

    Add code to change colour of cells

--- Assets/Scripts/GridVisualiser.cs
+++ Assets/Scripts/GridVisualiser.cs
@@ -10,12 +10,15 @@ public class GridVisualiser : MonoBehaviour
     Grid grid;
 
     GameObject[] drawnObjects;
+    static MaterialPropertyBlock block;
 
     // Load the visualiser with data
     public void Initialise(GameObject prefab, Grid grid)
     {
         this.prefab = prefab;
         this.grid = grid;
+
+        block = new MaterialPropertyBlock();
     }
 
     // Draws the grid 
@@ -27,8 +30,17 @@ public class GridVisualiser : MonoBehaviour
         {
             GameObject obj = Instantiate(prefab);
             obj.transform.localPosition = Vector3.right * i;
+            ApplyCellColour(obj);
             drawnObjects[i] = obj;
         }
+    }
+
+    void ApplyCellColour(GameObject obj)
+    {
+        var renderer = obj.GetComponent<Renderer>();
+        renderer.GetPropertyBlock(block);
+        block.SetColor("_BaseColor", Random.ColorHSV());
+        renderer.SetPropertyBlock(block);
     }
 }
 
