4ceddc510994112b43d35ac9edf83062cc4f6e9c
Faisa-l <146775603+Faisa-l@users.noreply.github.com>
2026-04-15 16:14:29 +0100

    Create a cell inspector
    Uses reflection and the right click input to display the fields of the cell the mouse is over. Should have implemented this wayyyy earlier

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,7 +1,7 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
-
 
 /// <summary>
 /// Handles the logic for the entire game
@@ -16,6 +16,12 @@ public class Game : MonoBehaviour
 
     [SerializeField]
     PlayerInputDirection inputDirection;
+
+    [SerializeField]
+    TextMeshProUGUI inspectorLabel;
+
+    [SerializeField]
+    bool cellInspectorEnabled = true;
 
     // Bind to an event to proceed the tick of the automaton
     public void ProceedTick(CallbackContext context)
@@ -54,5 +60,39 @@ public class Game : MonoBehaviour
     {
         var input = context.ReadValue<Vector2>();
         inputDirection.input = input;
+    }
+
+    public void ToggleGridInspector(CallbackContext context)
+    {
+        if (!cellInspectorEnabled) return; 
+
+        if (context.phase == InputActionPhase.Started)
+        {
+            InspectCell();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            inspectorLabel.text = "";
+        }
+    }
+
+    // Uses reflection to output the names of fields and their value
+    void InspectCell()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (!automaton.Visuasliser.TryGetTouchedCellIndex(ray, out int index)) return;
+
+        Cell cell = automaton.Grid[index];
+        automaton.Grid.GetRowColumn(index, out int row, out int column);
+        string output = "";
+        var cellFieldInfo = typeof(Cell).GetFields();
+
+        // Build string to display 
+        output += $"index (row, column) : {index} ({row}, {column})\n";
+        for (int i = 0; i < cellFieldInfo.Length; i++)
+        {
+            output += $"{cellFieldInfo[i].Name} : {cellFieldInfo[i].GetValue(cell)}\n";
+        }
+        inspectorLabel.text = output;
     }
 }
